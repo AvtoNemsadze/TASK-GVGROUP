187fd915819be7bd496c2cee95be56efc96950e9
AvtandilNemsadze <a.nemsadze@lemondo.biz>
2025-11-03 23:19:26 +0400

    add

--- SlotGameServer.API/Controllers/SpinController.cs
+++ SlotGameServer.API/Controllers/SpinController.cs
@@ -29,71 +29,26 @@ namespace SlotGameServer.API.Controllers
             _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
         }
 
-        /// <summary>
-        /// Initiates a new game session and returns the session ID.
-        /// </summary>
-        /// <param name="command">The command containing necessary information to start a new game session.</param>
-        /// <returns>Returns a newly created game session ID.</returns>
+       
         [HttpPost("start")]
         [Authorize(AuthenticationSchemes = "Bearer")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
-        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> StartGame(StartGameCommand command)
         {
             var result = await _mediator.Send(command);
             return Ok(new { result.SessionId });
         }
 
-        /// <summary>
-        /// Processes a spin in the casino game, checking user's balance, performing the spin, and updating user statistics.
-        /// </summary>
-        /// <param name="model">Spin command parameters including bet amount, chosen number, and session ID.</param>
-        /// <returns>Returns the result of the spin including win/loss information and any winnings.</returns>
-        [HttpPost("spin")]
+        
+        [HttpPost("spins")]
         [Authorize(AuthenticationSchemes = "Bearer")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SpinResult))]
-        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Spin([FromBody] SpinCommandModel model)
+        public async Task<ActionResult<SpinResult>> Spin([FromBody] SpinCommand command)
         {
-            var validator = new SpinCommandModelValidator();
-            var validationResult = validator.Validate(model);
-
-            if (!validationResult.IsValid)
-            {
-                var errors = validationResult.Errors.Select(e => new
-                {
-                    e.PropertyName,
-                    e.ErrorMessage
-                });
-                return BadRequest(errors);
-            }
-
-            var currentUserId = ClaimsPrincipalExtensions.GetUserId(_httpContextAccessor);
-
-            var command = new SpinCommand
-            {
-                BetAmount = model.BetAmount,
-                ChosenNumber = model.ChosenNumber,
-                SessionId = model.SessionId,
-                CreateUserId = currentUserId.GetValueOrDefault(),
-            };
-
             var result = await _mediator.Send(command);
             return Ok(result);
         }
 
-        /// <summary>
-        /// Retrieves all game bets with pagination.
-        /// </summary>
-        /// <param name="parameter">The pagination parameters.</param>
-        /// <returns>A paginated list of game bet entities.</returns>
+        
         [HttpGet]
-        [ProducesResponseType(typeof(PaginatedResponse<GetGameBetsListModel>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<PaginatedResponse<GetGameBetsListModel>>> GetAll([FromQuery] GameBetsListQueryParameter parameter)
         {
             var query = new GetAllGameBetsQuery(parameter.PageNumber, parameter.PageSize);
@@ -101,15 +56,8 @@ namespace SlotGameServer.API.Controllers
             return Ok(result);
         }
 
-        /// <summary>
-        /// Retrieves a game bet by its ID.
-        /// </summary>
-        /// <param name="id">The ID of the game bet to retrieve.</param>
-        /// <returns>The game bet entity with the specified ID.</returns>
+     
         [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById(int id)
         {
             var query = new GetGameBetsDetailsQuery(id);
--- SlotGameServer.API/Startup.cs
+++ SlotGameServer.API/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using MediatR;
+using Microsoft.OpenApi.Models;
 using SlotGameServer.API.Middleware;
 using SlotGameServer.Application;
 using SlotGameServer.Application.SignalR;
